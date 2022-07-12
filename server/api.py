6d8dc4eeab13d344d44d6280b732b98c35c77edb
lvillis <56720445+lvillis@users.noreply.github.com>
2022-07-13 01:44:36 +0800

    perf: CORS

--- server/api.py
+++ server/api.py
@@ -13,10 +13,9 @@ from utils import get_local_json
 app = FastAPI()
 
 # CORS
-origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
